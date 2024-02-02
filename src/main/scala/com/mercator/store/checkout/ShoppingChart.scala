ee3bdda676bda630b12feb8a39c62dba26ca793a
Prasanta Sinha <prasanta.sinha@paconsulting.com>
2024-02-02 13:21:19 +0000

    Step-2: Applied discount on apple and oranges.

--- src/main/scala/com/mercator/store/checkout/ShoppingChart.scala
+++ src/main/scala/com/mercator/store/checkout/ShoppingChart.scala
@@ -1,17 +1,20 @@
 package com.mercator.store.checkout
 
-trait Item {def price: Double}
-case class Apple(price: Double) extends Item
-case class Orange(price: Double) extends Item
+trait Item {def price: BigDecimal}
+case class Apple(price: BigDecimal) extends Item
+case class Orange(price: BigDecimal) extends Item
 
 trait ShoppingChart {
-  def total(): Double
+  def total(): BigDecimal
 }
 
 class ShoppingCart(items: List[Item]) extends ShoppingChart {
 
-  override def total(): Double = items.map(_.price).sum
-
+  val buyOneGeOneDiscountOnApple = buyOneGetOneDiscount(Apple(0.60))
+  val threeForTwoDiscountOnOrange = threeForTwoDiscount(Orange(0.25))
+  override def total() = items.map(_.price).sum - (buyOneGeOneDiscountOnApple + threeForTwoDiscountOnOrange)
+  private def buyOneGetOneDiscount(item: Item) = items.count(i => i == item) / 2 * item.price
+  private def threeForTwoDiscount(item: Item) = items.count(i => i == item) / 3 * 2 * item.price
 }
 
 object ShoppingCart {
--- src/test/scala/com/mercator/store/checkout/ShoppingCartSpec.scala
+++ src/test/scala/com/mercator/store/checkout/ShoppingCartSpec.scala
@@ -7,8 +7,16 @@ class ShoppingCartSpec extends AnyWordSpec {
 
   "A ShoppingCart" should {
     "return total cost of item list" in {
+      val itemList = List(Apple(0.60), Apple(0.60), Orange(0.25), Orange(0.25), Orange(0.25), Apple(0.60))
+      ShoppingCart(itemList).total mustEqual 1.45
+    }
+    "apply buy-one-get-one on apple" in {
       val itemList = List(Apple(0.60), Apple(0.60), Orange(0.25), Apple(0.60))
-      ShoppingCart(itemList).total mustEqual 2.05
+      ShoppingCart(itemList).buyOneGeOneDiscountOnApple mustEqual 0.60
+    }
+    "apply three for two on oranges" in {
+      val itemList = List(Apple(0.60), Apple(0.60), Orange(0.25), Orange(0.25), Orange(0.25), Orange(0.25), Apple(0.60))
+      ShoppingCart(itemList).threeForTwoDiscountOnOrange mustEqual 0.50
     }
   }
 
